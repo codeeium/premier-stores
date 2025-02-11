32cd0e4fcf4077f9f8f153f5a21dbb5381bd6199
codeeium <codeeium@gmail.com>
2025-02-11 21:51:41 +0000

    git ignore added

---
+++ premier-stores/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2025 Premier Stores</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ premier-stores/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Premier Stores</h1>
+            <p>Your favorite neighborhood store.</p>
+        </div>
+    );
+}
+
+export default Home;
