f51a214a4b02fcd1dbf71a8d5a94dca0a3ffaa84
codeeium <codeeium@gmail.com>
2025-02-12 17:38:06 +0000

    header image updated

--- premier-stores/src/components/Header.js
+++ premier-stores/src/components/Header.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'; // Import CSS file
+import premierLogo from '../header/premierLogo.png'; // Import logo
 
 function Header() {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
+        <header className="header">
+            <div className="header-content">
+                <img
+                    src={premierLogo}
+                    alt="Premier Logo"
+                    className="logo"
+                />
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
